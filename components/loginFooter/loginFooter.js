73f5bcf9f01da596fd90c38532c0ebe718e85d83
Peluo Rojas <peluo@MacBook-Pro-de-Andres.local>
2023-02-15 16:58:31 +0100

    responsive login

--- components/loginFooter/loginFooter.js
+++ components/loginFooter/loginFooter.js
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <div>
-            <ul className="absolute h-12  text-sm bottom-0 left-0 w-full flex justify-center items-center gap-5">
+            <ul className="absolute h-12 text-sm text-white md:text-black bottom-0 left-0 w-full flex justify-center items-center gap-5">
                 <li><a href="#">Privacy Policy</a></li>
                 <li><a href="#">Terms of Use</a></li>
                 <li><a href="#">Lorem Ipsum</a></li>
--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios"
 import { useRouter } from "next/router";
-import Footer from "/components/footer/footer";
+import Footer from "/components/loginFooter/loginFooter";
 
 function LoginPage() {
     const [credentials, setCredentials] = useState({ email: '', password: '' })
@@ -26,14 +26,13 @@ function LoginPage() {
         console.log(credentials)
     }
     return (
-        <div className="h-screen flex flex-row items-stretch columns-2">
-            <div className="login-hero relative w-3/6 flex justify-center items-center bg-accent">
-
+        <div className="h-screen flex flex-col md:flex-row justify-center items-stretch columns-1 md:columns-2  bg-sm-login">
+            <div className="login-hero relative w-full md:w-3/6 flex justify-center items-center bg-accent">
             </div>
-            <div className="relative h-full w-3/6 flex flex-col justify-center items-center">
+            <div className="static md:relative h-3/6 bg-white md:h-full w-11/12 md:w-3/6 flex mx-auto flex-col justify-center items-center rounded-md md:rounded-none">
                 <h1 className="text-secondary text-4xl font-bold">LOREM IPSUM</h1>
-                <p className="w-3/6 my-5 text-center mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                <form onSubmit={handleSubmit} className="w-3/6 flex flex-col gap-5">
+                <p className="w-5/6 md:w-4/6 lg:w-3/6 my-5 text-center mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <form onSubmit={handleSubmit} className="w-5/6 md:w-4/6 lg:w-3/6 flex flex-col justify-center items-stretch gap-5">
                     <input name="email" type="email" placeholder="email"
                         onChange={handleChange} />
                     <input name="password" type="password" placeholder="password"
