40b753059c2ddbf5f23279c5b5e75d7e90b0a3b9
Peluo Rojas <peluo@MacBook-Pro-de-Andres.local>
2023-02-15 16:08:30 +0100

    login frontend

---
+++ components/footer/footer.js
@@ -0,0 +1,13 @@
+function Footer(){
+    return(
+        <div>
+            <ul className="absolute h-12  text-sm bottom-0 left-0 w-full flex justify-center items-center gap-5">
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms of Use</a></li>
+                <li><a href="#">Lorem Ipsum</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer
--- pages/login.js
+++ pages/login.js
@@ -1,36 +1,47 @@
 import { useState } from "react";
-import axios from "axios"  
+import axios from "axios"
 import { useRouter } from "next/router";
-function LoginPage(){
-        const [credentials, setCredentials] = useState({email:'' , password:''})
-        const router = useRouter()
-        const handleChange = (event) =>{
-            setCredentials({
-                ...credentials,
-                [event.target.name] : event.target.value
-            })
-        }
-        const handleSubmit = (event) => {
-            event.preventDefault();
-            axios.post('/api/login', {
-                credentials
-            }).then((response) => {
-                
+import Footer from "/components/footer/footer";
 
-                router.push("/dashboard")
-                console.log(response)})
-            .catch((err) => console.log(err.toJSON()));            
-            console.log(credentials)
-        } 
-    return(
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input name="email" type="email" placxeholder="email"
-                onChange={handleChange}/>
-                <input name="password" type="password" placxeholder="password"
-                onChange={handleChange}/>
-                <button type="submit">Login</button>
-            </form>
+function LoginPage() {
+    const [credentials, setCredentials] = useState({ email: '', password: '' })
+    const router = useRouter()
+    const handleChange = (event) => {
+        setCredentials({
+            ...credentials,
+            [event.target.name]: event.target.value
+        })
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('/api/login', {
+            credentials
+        }).then((response) => {
+
+
+            router.push("/dashboard")
+            console.log(response)
+        })
+            .catch((err) => console.log(err.toJSON()));
+        console.log(credentials)
+    }
+    return (
+        <div className="h-screen flex flex-row items-stretch columns-2">
+            <div className="login-hero relative w-3/6 flex justify-center items-center bg-accent">
+
+            </div>
+            <div className="relative h-full w-3/6 flex flex-col justify-center items-center">
+                <h1 className="text-secondary text-4xl font-bold">LOREM IPSUM</h1>
+                <p className="w-3/6 my-5 text-center mx-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <form onSubmit={handleSubmit} className="w-3/6 flex flex-col gap-5">
+                    <input name="email" type="email" placeholder="email"
+                        onChange={handleChange} />
+                    <input name="password" type="password" placeholder="password"
+                        onChange={handleChange} />
+                    <button type="submit" className="btn">Login</button>
+                </form>
+                <Footer />
+            </div>
         </div>
     )
 }
