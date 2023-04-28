fae7f63ac04fa9c83b209acd27ffa561da95f575
Diego Gutua <diego.g0912@hormail.es>
2023-04-28 16:51:50 -0500

    formulario de contraseña y de login

--- components/recoveryComponent/recoveryComponent.js
+++ components/recoveryComponent/recoveryComponent.js
@@ -2,25 +2,12 @@
 import { useEffect, useState } from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
-import Loading from "../components/Loading/loading";
-import { useRouter } from 'next/router';
-import RecoveryForm from '../components/recoveryComponent/recoveryComponent';
 
-const logo = require("../src/images/logo.svg")
-
-export default function Recover() {
-    const router = useRouter()
-    const { id } = router.query
-    const [data,setData] = useState({password:""})
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            setIsLoading(false);
-        }, 1500);
-
-        return () => clearTimeout(timeoutId);
-    }, []);
+const logo = require("../../src/images/logo.svg")
+logo
+export default function RecoveryForm({toggleForm}) {
+    const [email,setEmail] = useState()
+        
     const handleSubmit = ()=>{
 
     }
@@ -32,9 +19,7 @@ export default function Recover() {
 
     return (
         <>
-            {isLoading ? (
-                <Loading />
-            ) : (<div className="relative h-screen flex flex-col md:flex-row justify-center items-stretch columns-1 md:columns-2  bg-login-hero bg-cover bg-center md:bg-white">
+            (<div className="relative h-screen flex flex-col md:flex-row justify-center items-stretch columns-1 md:columns-2  bg-login-hero bg-cover bg-center md:bg-white">
                 <div className="bg-recover bg-cover bg-no-repeat bg-center lg:bg-top-lg xl:bg-top-xl relative w-full md:w-3/6 flex justify-center items-center bg-accent">
                 </div>
                 <div className="static md:relative h-6/6 py-8 bg-white md:h-full w-11/12 md:w-3/6 flex mx-auto flex-col justify-center items-center rounded-md md:rounded-none">
@@ -47,28 +32,22 @@ export default function Recover() {
                     </Link>
                     <div className="w-11/12 md:w-6/12 flex flex-col gap-3">
                         <h1 className='text-center text-4xl font-bold'>Recupera tu contraseña</h1>
-                        <p className='text-center mb-3'>Digita nueva contraseña:</p>
+                        <p className='text-center mb-3'>Ingresa tu correo electrónico:</p>
                         <form onSubmit={handleSubmit} className='w-full flex flex-col justify-center gap-3'>
-                        <input
-                                type="password"
-                                placeholder="Constraseña"
+                            {<input
+                                type="email"
+                                placeholder="correo electrónico"
                                 className='w-full'
                                 required
                                 onChange={handleChange}
-                            ></input>
-                            <input
-                                type="password"
-                                placeholder="Confirmar contraseña"
-                                className='w-full'
-                                required
-                                onChange={handleChange}
-                            ></input>
+                            ></input>}
                             <button type='submit' className='w-48 mx-auto bg-primary rounded-md px-6 py-2 font-bold text-white hover:bg-dark'>Enviar</button>
                         </form>
+                        <button onClick={toggleForm} className="text-sm text-primary w-full text-center md:text-center">Login</button>
                     </div>
                 </div>
             </div>
-            )}
+            )
         </>
     );
 }
